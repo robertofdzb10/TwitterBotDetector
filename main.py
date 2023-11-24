da6461efd8f256b65d6bf75bf123ce7785727fba
robertofdzb10 <roberto.fernandez@opendeusto.es>
2023-11-24 11:44:13 +0100

    Update

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
-import tweepy
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-def is_bot(user):
-    # Add your Twitter API credentials here
-    consumer_key = "YOUR_CONSUMER_KEY"
-    consumer_secret = "YOUR_CONSUMER_SECRET"
-    access_token = "YOUR_ACCESS_TOKEN"
-    access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+# Inicializar el driver de Selenium
+driver = webdriver.Chrome()
 
-    # Authenticate with Twitter API
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
+# Navegar a la página de Twitter
+driver.get("https://twitter.com")
 
-    # Get user information
-    user_info = api.get_user(user)
+# Esperar a que se cargue la página
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.NAME, "session[username_or_email]")))
 
-    # Check if the user has a default profile image and a high number of followers
-    if user_info.default_profile_image and user_info.followers_count > 1000:
-        return True
-    else:
-        return False
+# Iniciar sesión en Twitter
+username_input = driver.find_element(By.NAME, "session[username_or_email]")
+password_input = driver.find_element(By.NAME, "session[password]")
+
+username_input.send_keys("tu_usuario")
+password_input.send_keys("tu_contraseña")
+
+login_button = driver.find_element(By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]')
+login_button.click()
+
+# Esperar a que se cargue la página de inicio
+wait.until(EC.presence_of_element_located((By.XPATH, '//div[@data-testid="tweet"]')))
+
+# Obtener los tweets en la página de inicio
+tweets = driver.find_elements(By.XPATH, '//div[@data-testid="tweet"]')
+
+# Analizar los tweets para detectar bots
+for tweet in tweets:
+    # Aquí puedes implementar tu lógica para detectar bots
+    # Por ejemplo, puedes verificar si el tweet contiene ciertas palabras clave o si el usuario tiene un número excesivo de seguidores
+
+    # Imprimir el contenido del tweet
+    print(tweet.text)
+
+
+
+# Cerrar el navegador
+driver.quit()
+
+#sleep randons de 1 a 5 s
